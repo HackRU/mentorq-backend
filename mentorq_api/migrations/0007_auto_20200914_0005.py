14acaeb81d1cbc0707b7bed8a2d36c75e6d6c09a
srnazng <srnazng@gmail.com>
2020-09-23 15:49:20 -0400

    clean up

--- mentorq_api/migrations/0007_auto_20200914_0005.py
+++ mentorq_api/migrations/0007_auto_20200914_0005.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('mentorq_api', '0006_auto_20200913_2329'),
+        ('mentorq_api', '0004_auto_20200809_2205'),
     ]
 
     operations = [
